1e1021b193fb73f964ae8ebf4ad5de3f14bab706
Sjoer van der Ploeg <sfjuocekr@gmail.com>
2015-12-23 20:51:05 +0100

    Example

---
+++ MIDI_tonePlay/MIDI_tonePlay.ino
@@ -0,0 +1,38 @@
+#include <MIDI.h>
+#include <Time.h>
+#include <TimeAlarms.h>
+
+#define LED 13
+
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+
+  Alarm.timerRepeat(2, sendNoteOn);
+}
+
+void sendNoteOn()
+{
+  digitalWrite(LED, HIGH);
+  
+  MIDI.sendNoteOn(60, 127, 1);
+  
+  Alarm.timerOnce(1, sendNoteOff);
+}
+
+void sendNoteOff()
+{
+  digitalWrite(LED, LOW);
+  
+  MIDI.sendNoteOff(60, 0, 1);
+}
+
+void loop()
+{
+  MIDI.read();
+  Alarm.delay(0);
+}
